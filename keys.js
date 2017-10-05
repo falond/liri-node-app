badb273741f8963b65036d554f1e7e046d9cbd8c
DESARE GAGO <desaregago@DESAREs-MacBook-Pro.local>
2017-10-05 01:54:28 -0400

    update js

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'uroWQ0gBJR75TnmmYfC2a0nr8',
+  consumer_secret: '7cL6ahdwXoHvnAjHwMsaBk68hRSvqH0AIrBiazlZQ2IKaYbII5',
+  access_token_key: '913252348109221888-YHbbmfT4FuKzk968DPOLO71Vrn3UnuU',
+  access_token_secret: 'WUWpwomFcSvmMtGtBbKcWoXkf0hCMc3PIR258M5YavrQ5',
 }
 
 module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,185 @@
+//Twitter Keys
+var tKeys = require("./keys.js");
+//Twitter NPM
+var Twitter = require("twitter");
+//Spotify NPM
+var Spotify = require("node-spotify-api");
+//omdb NPM
+var request = require("request");
+//fs package
+var fs = require('fs');
+// Takes in all of the command line arguments
+var inputString = process.argv;
+var command = inputString[2];
+//index three input
+var m ="";
+
+
+		//for loop for input string
+		for (var i=3; i<inputString.length; i++){
+		  if(i>3 && i<inputString.length){
+		    m = m + "+" + inputString[i];
+		  } else{
+		    m = m + inputString[i];
+
+		  }
+		}
+// ***************************DO WHAT IT SAYS************************************************
+			function doWhat(){
+			  fs.readFile('random.txt', "utf8", function(error, data){
+			  	 if (error) {
+    			return console.log(error);
+  				}
+			    var txt = data.split(',');
+			    spotify(txt[1]);
+
+			  });
+		
+			}
+
+//****************************switch case**************************************************
+		switch(command){
+
+		  case "my-tweets":
+		    twitter();
+		  break;
+
+		  case "spotify-this-song":
+		    if(m){
+		      spotify(m);
+		    } else{
+		      spotify("\"The Sign\" by Ace of Base");
+		    }
+		  break;
+
+		  case "movie-this":
+		    if(m){
+		      omdb(m);
+		    } else{
+		      omdb("Mr. Nobody");
+		    }
+		  break;
+
+		  case "do-what-it-says":
+		    if(m){
+		      doWhat(m);
+		    } else{
+		      doWhat("error");
+		    }
+		  break;
+		}
+
+// ***************************TWITTER****************************************************
+        //Twitter Keys
+		function twitter(){
+		var client = new Twitter({
+		  consumer_key: tKeys.consumer_key,
+		  consumer_secret: tKeys.consumer_secret,
+		  access_token_key: tKeys.access_token_key,
+		  access_token_secret: tKeys.access_token_secret
+		});
+		 //Parameters
+		var params = {screen_name: 'deslesno', count: 20};
+		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		  if(error) throw error;
+		  //loop through the tweets
+		  for (var i = 0; i < tweets.length; i++){
+		  //display date and tweet
+		     console.log("----------------------------------------------------------------------------");
+		     console.log("\nDate and Time: " + tweets[i].created_at);
+		     console.log("\nTweet: " + tweets[i].text);
+
+		     fs.appendFileSync('log.txt', "\nDate and Time: " + tweets[i].created_at);
+		     fs.appendFileSync('log.txt', "\nTweet: " + tweets[i].text);
+             fs.appendFileSync('log.txt', "\n---------------------------------------------------------------------------------------------------------------------------\n");
+		  }
+
+
+		});
+	};
+
+// ***************************SPOTIFY************************************************
+
+		function spotify(song){
+			var spotify = new Spotify({
+		    id: "860ea86e6246496a988f937952cc13e1",
+		    secret: "7a92cc533df146b5a05eede19cf4aa9e"
+		});
+
+			spotify.search({ type: 'track', query: song}, function(err, data) {
+		  if (err) {
+		    return console.log('Error occurred: ' + err);
+		  }
+		   for (var i = 0; i < data.tracks.items.length; i++){
+		  	    var songsInfo = data.tracks.items[i];
+
+		  	    //artist
+		        console.log("Artist: " + songsInfo.artists[0].name);
+		        //song name
+		        console.log("Song: " + songsInfo.name);
+		        //spotify preview link
+		        console.log("Preview URL: " + songsInfo.preview_url);
+		        //album name
+		        console.log("Album: " + songsInfo.album.name);
+		        console.log("-----------------------");
+
+		         //adds text to log.txt
+		        fs.appendFile('log.txt', songsInfo.artists[0].name);
+		        fs.appendFile('log.txt', songsInfo.name);
+		        fs.appendFile('log.txt', songsInfo.preview_url);
+		        fs.appendFile('log.txt', songsInfo.album.name);
+		        fs.appendFile('log.txt', "\n---------------------------------------------------------------------------------------------------------------------------\n");
+		  }
+		  
+
+		});
+
+	};
+
+// ***************************OMDB************************************************
+
+		function omdb(movie){
+		  var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&plot=short&apikey=40e9cece';
+		  request(omdbURL, function (error, response, body){
+		    if(!error && response.statusCode == 200){
+		      var body = JSON.parse(body);
+		      console.log("Title: " + body.Title);
+		      console.log("Year Released: " + body.Year);
+		      console.log("IMDB Rating of Movie: " + body.imdbRating);
+		      console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+		      console.log("Country Produced in: " + body.Country);
+		      console.log("Language: " + body.Language);
+		      console.log("Plot: " + body.Plot);
+		      console.log("Actors: " + body.Actors);
+		      
+		     
+		      //adds text to log.txt
+		      fs.appendFileSync('log.txt', "\nTitle: " + body.Title);
+		      fs.appendFileSync('log.txt', "\nYear Released: " + body.Year);
+		      fs.appendFileSync('log.txt', "\nIMDB Rating of Movie: " + body.imdbRating);
+		      fs.appendFileSync('log.txt', "\nRotten Tomatoes Rating: " + body.tomatoRating);
+		      fs.appendFileSync('log.txt', "\nCountry Produced in: " + body.Country);
+		      fs.appendFileSync('log.txt', "\nLanguage: " + body.Language);
+		      fs.appendFileSync('log.txt', "\nPlot: " + body.Plot);
+		      fs.appendFileSync('log.txt', "\nActors: " + body.Actors);
+		      fs.appendFileSync('log.txt', "\n---------------------------------------------------------------------------------------------------------------------------\n");
+		    }
+		    
+		    if(error) throw error;
+		    if(movie === "Mr. Nobody"){
+		      console.log("-----------------------");
+		      console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+		      console.log("It's on Netflix!");
+		      //adds text to log.txt
+		      fs.appendFileSync('log.txt', "-----------------------");
+		      fs.appendFileSync('log.txt', "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+		      fs.appendFileSync('log.txt', "It's on Netflix!");
+    }
+  });
+}
+
+
+
+
+
+
